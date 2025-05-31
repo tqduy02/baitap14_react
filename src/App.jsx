b5a652035670118207ce2e7e5914ddb4b614405d
duyt02 <tqduy02@gmail.com>
2025-05-31 10:46:56 +0700

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,15 @@
-import { useEffect, useState } from "react";
 import BookList from "./components/BookList";
 import BookCreate from "./components/BookCreate";
-import {
-  fetchBooks,
-  createBook,
-  deleteBookById,
-  editBookById
-} from "./Api";
+import useBooks from "./hooks";
 import './App.css';
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    const loadBooks = async () => {
-      const books = await fetchBooks();
-      setBooks(books);
-    };
-    loadBooks();
-  }, []);
-
-  const handleCreateBook = async (title) => {
-    const newBook = await createBook(title);
-    setBooks([...books, newBook]);
-  };
-
-  const handleDeleteBook = async (id) => {
-    await deleteBookById(id);
-    setBooks(books.filter((book) => book.id !== id));
-  };
-
-  const handleEditBook = async (id, newTitle) => {
-    const updatedBook = await editBookById(id, newTitle);
-    const updatedBooks = books.map((book) =>
-      book.id === id ? { ...book, ...updatedBook } : book
-    );
-    setBooks(updatedBooks);
-  };
+  const {
+    books,
+    handleCreateBook,
+    handleDeleteBook,
+    handleEditBook
+  } = useBooks();
 
   return (
     <div className="app">
